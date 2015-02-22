2fb77f37f8ec4f53d6b104173d4bd7d467821586
Bakum Viacheslav <bakum.v@gmail.com>
2015-02-22 23:01:43 +0200

    Entry orders repair

--- src/java/ua/divas/mob/util/DivasEntry.java
+++ src/java/ua/divas/mob/util/DivasEntry.java
@@ -8,12 +8,13 @@ package ua.divas.mob.util;
 import java.io.Serializable;
 import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.Query;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import ua.divas.mob.entity.Orders;
 
 /**
@@ -25,22 +26,20 @@ public class DivasEntry implements Serializable {
 //    //resource injection
 //    @Resource(name = "jdbc/divas_mobiDS")
 //    private static DataSource ds;
-//    public static void entryOrders(Orders order) throws SQLException {
+//    public static void entryOrdersJndi(Orders order) throws SQLException, NamingException {
+//        Context ctx = new InitialContext();
+//        DataSource ds = (DataSource) ctx.lookup("jdbc/divas_mobiDS");
 //        if (ds == null) {
 //            throw new SQLException("Can't get data source");
 //        }
-//
 //        //get database connection
 //        Connection con = ds.getConnection();
-//
 //        if (con == null) {
 //            throw new SQLException("Can't get database connection");
 //        }
-//        
 //        String Id = order.getId();
-//
 //        PreparedStatement ps
-//                = con.prepareStatement("begin ORDERS_ENTRY.ORDERS_MOVE_PLAN_ACC('"+Id+"') end;");
+//                = con.prepareStatement("begin ORDERS_ENTRY.ORDERS_MOVE_PLAN_ACC('" + Id + "'); end;");
 //        ps.executeQuery();
 //
 //    }
@@ -61,34 +60,47 @@ public class DivasEntry implements Serializable {
 //        }
 //
 //    }
-    private static final String DB_DRIVER = "oracle.jdbc.driver.OracleDriver";
-    private static final String DB_CONNECTION = "jdbc:oracle:thin:@192.168.0.200:1521:db11g";
-    private static final String DB_USER = "dba_divas";
-    private static final String DB_PASSWORD = "divas";
+//    private static final String DB_DRIVER = "oracle.jdbc.driver.OracleDriver";
+//    private static final String DB_CONNECTION = "jdbc:oracle:thin:@192.168.0.200:1521:db11g";
+//    private static final String DB_USER = "dba_divas";
+//    private static final String DB_PASSWORD = "divas";
 
-    private static Connection getDBConnection() {
+//    private static Connection getDBConnection() {
+//        Connection dbConnection = null;
+//        try {
+//            Class.forName(DB_DRIVER);
+//        } catch (ClassNotFoundException e) {
+//            System.out.println(e.getMessage());
+//        }
+//        try {
+//            dbConnection = DriverManager.getConnection(
+//                    DB_CONNECTION, DB_USER, DB_PASSWORD);
+//            return dbConnection;
+//        } catch (SQLException e) {
+//            System.out.println(e.getMessage());
+//        }
+//        return dbConnection;
+//    }
+
+    private static Connection getDBConnectionJndi() throws NamingException, SQLException {
         Connection dbConnection = null;
+        Context ctx = null;
         try {
-            Class.forName(DB_DRIVER);
-        } catch (ClassNotFoundException e) {
-            System.out.println(e.getMessage());
+            ctx = new InitialContext();
+        } catch (NamingException ex) {
+            Logger.getLogger(DivasEntry.class.getName()).log(Level.SEVERE, null, ex);
         }
-        try {
-            dbConnection = DriverManager.getConnection(
-                    DB_CONNECTION, DB_USER, DB_PASSWORD);
-            return dbConnection;
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+        DataSource ds = (DataSource) ctx.lookup("jdbc/divas_mobiDS");
+        dbConnection = ds.getConnection();
         return dbConnection;
     }
 
-    public static void entryOrders(Orders order) throws SQLException {
+    public static void entryOrders(Orders order) throws SQLException, NamingException {
         Connection dbConnection = null;
         CallableStatement callableStatement = null;
         String entryStoreProc = "begin ORDERS_ENTRY.ORDERS_MOVE_PLAN_ACC(?); end;";
         try {
-            dbConnection = getDBConnection();
+            dbConnection = getDBConnectionJndi();
             callableStatement = dbConnection.prepareCall(entryStoreProc);
             //System.out.println(order.getId());
             callableStatement.setString(1, order.getId());
